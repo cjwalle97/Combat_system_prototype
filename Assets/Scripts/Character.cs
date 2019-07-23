2d839a0214dfd76f009e21ede423a35be78ff517
cjwalle97 <cjwalle97@gmail.com>
2019-07-23 18:06:01 -0500

    7/23/19 Update
    A new Update where I restart the system from scratch

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : ScriptableObject
-{ 
-    
+{
+    private string name;
+
+    public int HP;
+    public int MP;
+    public Stats stats;
+
+
 }
---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : ScriptableObject
+{
+    public Stat Strength;
+    public Stat Soul;
+    public Stat Defense;
+    public Stat Will;
+    public Stat Agility;
+    public Stat Luck;
+}
